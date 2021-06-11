cce1bd32c64c62ef7e0e79bd2797dba60a45899c
Alexandra <alexadubinina@mail.ru>
2021-06-11 15:55:47 +0300

    Урок 6(RabbitMQ)Б  Домашняя работа(код)

--- src/main/java/consumer/ITMessagesDirectReceiver.java
+++ src/main/java/consumer/ITMessagesDirectReceiver.java
@@ -5,35 +5,35 @@ import com.rabbitmq.client.*;
 import java.util.Scanner;
 
 public class ITMessagesDirectReceiver {
-//    private static final String EXCHANGE_NAME = "ITArticles";
-//
-//    public static void main(String[] argv) throws Exception {
-//        ConnectionFactory factory = new ConnectionFactory();
-//        factory.setHost("localhost");
-//        Connection connection = factory.newConnection();
-//        Channel channel = connection.createChannel();
-//
-//        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-//
-//        String queueName = channel.queueDeclare().getQueue();
-//        System.out.println("My queue name: " + queueName);
-//
-//
-//        System.out.println("Please enter theme in format : set_topic (php/java/c++)");
-//        Scanner scanner = new Scanner(System.in);
-//        String theme = scanner.nextLine().split(" ",2)[1];
-//
-//
-//        channel.queueBind(queueName, EXCHANGE_NAME, theme);
-//
-//        System.out.println(" [*] Waiting for messages with theme " + theme);
-//
-//        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-//            String message = new String(delivery.getBody(), "UTF-8");
-//            System.out.println(" [x] Received '" + message + "'");
-//            System.out.println(Thread.currentThread().getName());
-//        };
-//
-//        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
-//    }
+    private static final String EXCHANGE_NAME = "ITArticles";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+        String queueName = channel.queueDeclare().getQueue();
+        System.out.println("My queue name: " + queueName);
+
+
+        System.out.println("Please enter theme in format : set_topic (php/java/c++)");
+        Scanner scanner = new Scanner(System.in);
+        String theme = scanner.nextLine().split(" ",2)[1];
+
+
+        channel.queueBind(queueName, EXCHANGE_NAME, theme);
+
+        System.out.println(" [*] Waiting for messages with theme " + theme);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+            System.out.println(Thread.currentThread().getName());
+        };
+
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
 }
--- src/main/java/producer/ITMessagesSenderApp.java
+++ src/main/java/producer/ITMessagesSenderApp.java
@@ -6,31 +6,31 @@ import java.util.Scanner;
 
 
 public class ITMessagesSenderApp {
-//
-//    private static final String EXCHANGE_NAME = "ITArticles";
-//
-//    public static void main(String[] argv) throws Exception {
-//        ConnectionFactory factory = new ConnectionFactory();
-//        factory.setHost("localhost");
-//        try (Connection connection = factory.newConnection();
-//             Channel channel = connection.createChannel()) {
-//            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-//
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.println("Enter article in format: topic(java/php/c++) text");
-//            System.out.println("For example: java some message");
-//            System.out.println("Enter 0 to quit");
-//            while (true) {
-//                String message = scanner.nextLine();
-//                if (message.equals("0")){
-//                    return;
-//                }
-//                String topic = message.split(" ", 2)[0];
-//                message = message.split(" ", 2)[1];
-//                channel.basicPublish(EXCHANGE_NAME, topic, null, message.getBytes("UTF-8"));
-//                System.out.println("OK");
-//                System.out.println("Enter new message or 0 - to quit");
-//            }
-//        }
-//    }
+
+    private static final String EXCHANGE_NAME = "ITArticles";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter article in format: topic(java/php/c++) text");
+            System.out.println("For example: java some message");
+            System.out.println("Enter 0 to quit");
+            while (true) {
+                String message = scanner.nextLine();
+                if (message.equals("0")){
+                    return;
+                }
+                String topic = message.split(" ", 2)[0];
+                message = message.split(" ", 2)[1];
+                channel.basicPublish(EXCHANGE_NAME, topic, null, message.getBytes("UTF-8"));
+                System.out.println("OK");
+                System.out.println("Enter new message or 0 - to quit");
+            }
+        }
+    }
 }
